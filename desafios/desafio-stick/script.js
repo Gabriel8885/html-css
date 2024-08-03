3edf326411575c54888712c7f2a1614ed334c54a
Gabriel8885 <gabrielnascimento8115@gmail.com>
2024-08-03 15:50:13 -0300

    desafio stick

--- desafios/desafio-stick/script.js
+++ desafios/desafio-stick/script.js
@@ -1,6 +1,7 @@
 function clickMenu(){
     if (items.style.display == 'none'){
         items.style.display = 'block';
+        burger.style.background = 'url(../imagens/menuaberto)';
     } else{
         items.style.display = 'none';
     }
